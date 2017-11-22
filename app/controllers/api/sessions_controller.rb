5db16565b3fd607e90dd76909885b58ef2495d09
Christina Kiesz <ckiesz@gmail.com>
2017-11-22 10:39:07 -0800

    re-add the frontend folders

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -16,7 +16,7 @@ class Api::SessionsController < ApplicationController
     @user = current_user
     if @user
       logout
-      render "/"
+      render "api/users/show"
     else
       render json: ["No current user"], status: 404
     end
--- frontend/800pics.jsx
+++ frontend/800pics.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as Session_API_Util from './util/session_api_util';
+// import * as Session_API_Util from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  window.login = Session_API_Util.login;
-  window.logout = Session_API_Util.logout;
-  window.createUser = Session_API_Util.signup;
   ReactDOM.render(<h2>Hi from the app</h2>, root);
 });
