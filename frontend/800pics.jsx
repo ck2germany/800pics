1c1e540a4cd87d9870884af3bb9d1f6950a8b7f1
Christina Kiesz <ckiesz@gmail.com>
2017-11-22 11:56:24 -0800

    finish session and session errors reducer, add store.js and import into entry file

--- frontend/800pics.jsx
+++ frontend/800pics.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import * as Session_API_Util from './util/session_api_util';
+import * as SessionAPIUtil from './util/session_api_util';
+
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h2>Hi from the app</h2>, root);
 });
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,4 +1,4 @@
-import combineReducers from 'redux';
+import { combineReducers } from 'redux';
 
 import errorsReducer from './errors_reducer';
 import sessionReducer from './session_reducer';
