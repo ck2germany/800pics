08da8a03f7032400bb55c06329cb347e1c3aedf8
Christina Kiesz <ckiesz@gmail.com>
2017-11-26 17:36:03 -0800

    add functional component for intro-splash content and split css files

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,6 +5,7 @@ import { AuthRoute } from '../util/route_util';
 
 import NavBarContainer from './navbar/navbar_container';
 import SessionFormContainer from './session/session_form_container';
+import Intro from './intro_splash/intro';
 
 //the App component is the only thing in root, but we use root to wrap
 // the app component in HashRouter and Provider
@@ -12,10 +13,13 @@ const App = () => (
   <div>
 
       <Route path="/" component={NavBarContainer} />
+
       <AuthRoute path="/login" component={SessionFormContainer} />
       <AuthRoute path="/signup" component={SessionFormContainer} />
+      <Route exact path="/" component={Intro} />
 
   </div>
 );
+
 
 export default App;
---
+++ frontend/components/intro_splash/intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => (
+  <div className="home">
+    <div className="splash-img-frame">
+      <img className="splash-img"
+        src="http://res.cloudinary.com/studio-27/image/upload/e_sharpen:100/v1511731707/landscapes/JK/jk_photos-bancroft1.jpg"
+        alt="splash-photo"/>
+    </div>
+    <h1>Landscapes: Reconnect to Nature</h1>
+
+  </div>
+)
