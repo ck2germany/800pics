8fa1c799eec68fdd0ffd05e85059590fbc33e47b
Christina Kiesz <ckiesz@gmail.com>
2017-11-26 22:31:48 -0800

    continue working on splash, need to align signup button

--- frontend/components/intro_splash/intro.jsx
+++ frontend/components/intro_splash/intro.jsx
@@ -4,11 +4,22 @@ import { Link } from 'react-router-dom';
 export default () => (
   <div className="home">
     <div className="splash-img-frame">
-      <img className="splash-img"
-        src="http://res.cloudinary.com/studio-27/image/upload/e_sharpen:100/v1511731707/landscapes/JK/jk_photos-bancroft1.jpg"
-        alt="splash-photo"/>
+      <img
+        className="splash-img"
+        src="http://res.cloudinary.com/studio-27/image/upload/v1511731666/landscapes/JD/jd_photos-idaho_3.jpg"
+        alt="splash"></img>
+        <div className="splash-img-text">
+          <h1>Landscapes, a website devoted to photography</h1>
+          <h3>Rediscover your love of nature</h3>
+
+
+        </div>
+        <div className="">
+            <Link className="splash-button" to='/signup'>Join Landscapes</Link>
+        </div>
+
     </div>
-    <h1>Landscapes: Reconnect to Nature</h1>
+
 
   </div>
 )
