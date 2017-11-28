95bf30c706c27d97d578b9e680d13f4a4a751236
Christina Kiesz <ckiesz@gmail.com>
2017-11-27 17:18:09 -0800

    added figaro for cloudinary, migrated photos table

---
+++ db/migrate/20171128004804_create_photos.rb
@@ -0,0 +1,13 @@
+class CreatePhotos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :photos do |t|
+      t.integer :user_id, null: false
+      t.string :title, null:false
+      t.string :description, default: ""
+      t.string :img_url, null: false
+      t.timestamps
+    end
+
+    add_index :photos, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121232759) do
+ActiveRecord::Schema.define(version: 20171128004804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.string "description", default: ""
+    t.string "img_url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
