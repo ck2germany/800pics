6c9af981e748a28ec0b6d6abe9b70c8344d2e23d
Christina Kiesz <ckiesz@gmail.com>
2017-11-27 18:26:10 -0800

    Add photos controller, start writing methods

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,2 +1,55 @@
 class Api::PhotosController < ApplicationController
+
+  def index
+    # have to decide to split indexes here or use selector in frontend
+
+  end
+
+  def show
+    @photo = Photo.find_by(params[:id])
+    if @photo
+      render "api/photos/show"
+    else
+      render json: ["Couldn't find photo"], status: 422
+    end
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.user_id = current_user.id
+    if @photo.save
+      render "api/photos/show"
+    else
+      render json: @photo.errors.full_messages, status: 422
+    end
+  end
+
+  def edit
+    @photo = Photo.find_by(params[:id])
+    if @photo.update_attributes(photo_edit_params)
+      render "api/photos/show"
+    else
+      render json: @photo.erros.full_messages, status: 422
+    end
+
+  end
+
+  def delete
+    @photo = Photo.find_by(params[:id])
+    if @photo
+      @photo.destroy
+      render "api/photos/show"
+    else
+      render json: ['Photo was not found, ergo not deleted']
+    end
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :description, :img_url)
+  end
+
+  def photo_edit_params
+    params.require(:photo).permit(:title, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:create, :destroy]
+    resources :photos, except: :new
   end
+
+
 end
