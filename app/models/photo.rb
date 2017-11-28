0bf6f8caa96c66cd688c90cbead3c24e89a3fa32
Christina Kiesz <ckiesz@gmail.com>
2017-11-27 17:28:24 -0800

    add photo.rb model validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,26 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  title       :string           not null
+#  description :string           default("")
+#  img_url     :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Photo < ApplicationRecord
+  validates :user_id, :title, :img_url, presence: true
+  validates :title, length: { maximum: 64 }
+  before_validation :ensure_description
+  def ensure_description
+    self.description ||= ""
+  end
+
+  belongs_to :user,
+  foreign_key: :user_id,
+  class_name: :User
+
 end
--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  title       :string           not null
+#  description :string           default("")
+#  img_url     :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
