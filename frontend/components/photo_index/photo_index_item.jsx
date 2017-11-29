c06f3de48a056ae3990ae179ac9d72dd30e0306c
Christina Kiesz <ckiesz@gmail.com>
2017-11-28 18:25:18 -0800

    work on photo feed

--- frontend/components/photo_index/photo_index_item.jsx
+++ frontend/components/photo_index/photo_index_item.jsx
@@ -5,14 +5,14 @@ const PhotoIndexItem = ({photo}) => {
   return (
     <li className="photo-index-item">
       <div className="photo-item-userbox">
-        <h3>Placeholder for User Info Box</h3>
+        <h2>Demo Usr</h2>
       </div>
       <div className="photo-item-imagebox">
         <img src={photo.img_url} alt="test image" />
       </div>
       <div className="photo-item-infobox">
         <h3>{photo.title}</h3>
-        <h4>{photo.description}</h4>
+        <h4></h4>
       </div>
   </li>
   );
