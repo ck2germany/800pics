23e09ac49c64208028d1d4fbc95b2d8b25f1660e
Christina Kiesz <ckiesz@gmail.com>
2017-11-29 14:24:03 -0800

    Start adding jbuilder and controller/routes to fetch user info

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,8 +10,17 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render "api/users/show"
+    else
+      render json: ["User not found"], status: 404
+    end
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :password)
   end
